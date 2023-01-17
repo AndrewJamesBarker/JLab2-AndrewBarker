8b6a5907643db8b7c6ae36374b774558fdcbc2a0
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-01-17 16:48:45 -0500

    basic site example with node express and pug

---
+++ index.js
@@ -0,0 +1,34 @@
+//import required modules
+
+const express = require("express"); 
+
+const path = require("path");
+
+//set up Express object and port
+
+const app = express();
+const port = process.env.PORT || "8888";
+
+//Set up template engine
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+//Set up static file paths
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+
+//page routes
+
+app.get("/", (req, res) => { 
+ res.render("index", {title: "Home"});
+});
+
+//set up server listening
+
+app.listen(port, () => {
+console.log(`Listening on http://localhost:${port}`);
+});
